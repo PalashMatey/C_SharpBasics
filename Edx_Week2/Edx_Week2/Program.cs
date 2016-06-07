8a06ef88060208d5e1084c5d1a91ba83d93daf3a
PalashMatey <pm2824@columbia.edu>
2016-06-07 11:25:31 -0500

    Updating Week2

---
+++ Edx_Week2/Edx_Week2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edx_Week2
+{
+    class Patterns
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 8; i++)
+            {
+                Console.WriteLine();
+
+                for (int j = 1; j <= 8; j++)
+                {
+                    if (i%2==0)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write("X");
+                    }
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
