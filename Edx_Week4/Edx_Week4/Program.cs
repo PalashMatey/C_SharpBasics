4f4b977d555e5e55993f45f4b3a5a28719561389
PalashMatey <pm2824@columbia.edu>
2016-06-07 16:56:47 -0500

    Started with Week4

---
+++ Edx_Week4/Edx_Week4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Jagged arrays are interesting. They are basically array of arrays
+/*
+ int[][] jaggedArray = new int [10][];
+ jaggedArray[0] = new Type[5];
+ jaggedArray[1] = new Type[7];
+ and so on
+*/
+namespace Edx_Week4
+{
+    class RelativelyComplexData
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
