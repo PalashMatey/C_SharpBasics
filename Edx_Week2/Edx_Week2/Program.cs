b0dcffb84a6ee8bd17fbf9cbe8a771bf6f9e854d
PalashMatey <pm2824@columbia.edu>
2016-06-07 12:48:59 -0500

    Better Version

--- Edx_Week2/Edx_Week2/Program.cs
+++ Edx_Week2/Edx_Week2/Program.cs
@@ -1,37 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Edx_Week2
+namespace chessboard
 {
-    class Patterns
+    class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 8; i++)
+            for (int column = 0; column < 8; column++) // will create 8 row x 8 column pattern
             {
-                Console.WriteLine();
-               
-                for (int j = 1; j <= 4; j++)
+                for (int row = 0; row < 8; row++)
                 {
-                    if (i % 2 == 0)
+                    if ((row + column) % 2 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else if ((row + column) % 2 == 1)
                     {
                         Console.Write("O");
-                        Console.Write("X");
-                        
                     }
                     else
                     {
-                        Console.Write("X");
-                        Console.Write("O");
+                        Console.Write("Neither X nor O"); // Built in unit test if logic for X and O fails
                     }
-
                 }
+                Console.WriteLine();
             }
             Console.ReadKey();
         }
     }
 }
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Policy;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Edx_Week2
+//{
+//    class Patterns
+//    {
+//        static void Main(string[] args)
+//        {
+//            for (int i = 1; i <= 8; i++)
+//            {
+//                Console.WriteLine();
+
+//                for (int j = 1; j <= 4; j++)
+//                {
+//                    if (i % 2 == 0)
+//                    {
+//                        Console.Write("O");
+//                        Console.Write("X");
+
+//                    }
+//                    else
+//                    {
+//                        Console.Write("X");
+//                        Console.Write("O");
+//                    }
+
+//                }
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//}
