2af5e36268cf721ba335da9f2d1e8bbfacb09bdb
PalashMatey <pm2824@columbia.edu>
2016-06-07 12:41:38 -0500

    Final Week2 Assignment

--- Edx_Week2/Edx_Week2/Program.cs
+++ Edx_Week2/Edx_Week2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,16 +14,19 @@ namespace Edx_Week2
             for (int i = 1; i <= 8; i++)
             {
                 Console.WriteLine();
-
-                for (int j = 1; j <= 8; j++)
+               
+                for (int j = 1; j <= 4; j++)
                 {
-                    if (i%2==0)
+                    if (i % 2 == 0)
                     {
                         Console.Write("O");
+                        Console.Write("X");
+                        
                     }
                     else
                     {
                         Console.Write("X");
+                        Console.Write("O");
                     }
 
                 }
