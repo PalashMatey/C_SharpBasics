81f75a8dad88ff4a0a95622794fe7b9caecf8ee7
PalashMatey <pm2824@columbia.edu>
2016-06-07 10:31:08 -0500

    Completed Week1 exercise

--- Edx_Week1/Edx_Week1/Program.cs
+++ Edx_Week1/Edx_Week1/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Edx_Week1
 {
-    class Program
+    class School
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string FirstName;
+            string LastName;
+            string Birthdate;
+            string AddressLine1;
+            string AddressLine2;
+            string City;
+            string State;
+            int ZipCode;
+            string Country;
+
+            FirstName = "Palash";
+            LastName = "Matey";
+            Birthdate = "6th June 2016";
+            AddressLine1 = "Something";
+            AddressLine2 = "Make this optional";
+            City = "New YOrk";
+            State = "New York";
+            ZipCode = 10027;
+            Country = "USA";
+            Console.WriteLine("{0}{1}",FirstName,LastName);
+            Console.WriteLine("{0} {1}",FirstName,LastName);
+            Console.WriteLine(Birthdate);
+            Console.WriteLine("{0} {1}", AddressLine1,AddressLine2);
+            Console.WriteLine(City);
+            Console.WriteLine(State);
+            Console.WriteLine(ZipCode);
+            Console.WriteLine(Country);
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+
         }
     }
 }
