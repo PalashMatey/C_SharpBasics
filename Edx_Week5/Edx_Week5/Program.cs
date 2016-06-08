4fbdec8b8d5d648ee37590dd160f999b52a3edc3
PalashMatey <pm2824@columbia.edu>
2016-06-08 15:46:18 -0500

    updating changes

--- Edx_Week5/Edx_Week5/Program.cs
+++ Edx_Week5/Edx_Week5/Program.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//use of partial classes to split the class over various source files which can all be combined during compile.
+//constructors have the same name as the class but they also have parameters which are used to initialize variables.
+//constructors can be used to directly initialize the variables at the time of object creation.
+//regardless of the instances of a class, there is always only one static member. Typically you do not need to instantiate a class in order to use a static member.
+//Anonymous classes, way of encapsulating read only properties into a single object without the need to explicitly define a type first.
+//However you cannot define methods for anonymous classes
 
 namespace Edx_Week5
 {
@@ -10,6 +16,9 @@ namespace Edx_Week5
     {
         static void Main(string[] args)
         {
+            #region Nothing important
+
+            #endregion
         }
     }
 }
---
+++ Edx_Week5/Edx_Week5/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edx_Week5
+{
+    class Student
+    {
+        public string StudentName { get; set; }
+        public string Birthdate { get; set; }
+
+        
+    }
+}
