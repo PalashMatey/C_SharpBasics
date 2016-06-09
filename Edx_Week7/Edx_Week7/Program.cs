adbb29bb7243d304a5b2465699f2ae72637bed55
PalashMatey <pm2824@columbia.edu>
2016-06-09 18:47:37 -0500

    Week 7 notes completed

---
+++ Edx_Week7/Edx_Week7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Collections is C#'s way of defining lists and dictionaries / Queue's and Stack's.
+ * This would be useful for algorithm and data structure implementation.
+ * Why use a sorted list(allows you to access items by key) when you already have a hashtable.
+ * something to do with performance but will investigate this further.
+ * Add objects of any kind to an ArrayList collection.
+ * Retrieving items from a collection must be done explicitly. 
+ * Its best to use a foreach loop to iterate over the collection.
+ * 
+*/
+namespace Edx_Week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
