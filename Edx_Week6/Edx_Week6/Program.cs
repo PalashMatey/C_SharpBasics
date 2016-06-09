9d4b6de80fca9914a562f528ab3a1244e18195a2
PalashMatey <pm2824@columbia.edu>
2016-06-09 15:47:36 -0500

    Documentation regarding abstract classes

--- Edx_Week6/Edx_Week6/Program.cs
+++ Edx_Week6/Edx_Week6/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*The difference between inheritance and adding an object to another class is that in inheritance, you can assume the 
+properties of the base class. It would not get added to your class or associated with it, just will make writing of the 
+code simpler as you will not have to add repeat properties to the class.
+Abstract classes are useful when you do not have to every use the base class. You just want to use the properties, and methods.
+Defining a method as virtual in the abstract class(base class) means that it can be overridden in the subclass. 
+This means that the subclasses can either accept the inherited implementation or accept the implementation that is inherited.
+If a method is declared as abstract in an abstract class it must be implemented in the subclass, else it would throw a compiler error.
+Abstract method is not permitted to have any implementation, read earlier statement.
+*/
 namespace Edx_Week6
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
