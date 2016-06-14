429df13ba95937c937c6ff416733251b7504da09
PalashMatey <pm2824@columbia.edu>
2016-06-14 17:15:26 -0500

    Adding sources of useful information for tomorrow!

---
+++ WebSocket_Test/WebSocket_Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+/* source : http://stackoverflow.com/questions/153451/how-to-check-if-system-net-webclient-downloaddata-is-downloading-a-binary-file#156750
+https://thuru.net/
+
+*/
+namespace WebSocket_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.
+            }
+        }
+    }
+}
