e11710a98d4e7098432b28f88eea5ba7c14e01a2
PalashMatey <pm2824@columbia.edu>
2016-06-14 17:47:53 -0500

    Checking if the Url string is valid or not

--- WebSocket_Test/WebSocket_Test/Program.cs
+++ WebSocket_Test/WebSocket_Test/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Security.Policy;
+
 /* source : http://stackoverflow.com/questions/153451/how-to-check-if-system-net-webclient-downloaddata-is-downloading-a-binary-file#156750
 https://thuru.net/
 
@@ -14,10 +17,16 @@ namespace WebSocket_Test
     {
         static void Main(string[] args)
         {
-            using (WebClient client = new WebClient())
-            {
-                client.Headers.
-            }
+            string urlname = "pos1.olostaging.com";
+
+
+            Uri uriResult;
+            bool result = Uri.TryCreate(urlname, UriKind.Absolute, out uriResult)
+                          && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            Console.WriteLine(result);
+            Console.WriteLine("Press key to continue");
+            Console.ReadKey();
         }
     }
 }
